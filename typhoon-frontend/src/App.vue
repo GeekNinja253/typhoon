ac1d3d9e3b5b50727a00302e3888cf532ed85cf2
GeekNinja253 <7035533qrv@gmail.com>
2026-06-07 14:53:28 +0800

    测试字段删除

--- typhoon-frontend/src/App.vue
+++ typhoon-frontend/src/App.vue
@@ -5,8 +5,5 @@ console.log("test git change")
 <template>
   <div id="app">
     <router-view />
-    <h1>TEST-GIT-</h1>
-    <h1>TEST_123456</h1>
-	<h1>HARD_WRITE_TEST_999</h1>
   </div>
 </template>
